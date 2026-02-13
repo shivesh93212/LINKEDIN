d884dddf9d2f8f367d8a7140d8c7725f779e6491
shivesh93212 <0105cd231185@oriental.ac.in>
2026-02-14 00:36:48 +0530

    final

---
+++ linkedin/src/components/Navbar/DesktopNavbar.jsx
@@ -0,0 +1,94 @@
+import React from "react"
+import {Link} from "react-router-dom"
+import{Search,Home,Users,BriefcaseBusiness,MessageSquareText,Bell,Grid3X3,} from "lucide-react"
+
+
+export default function DesktopNavbar(){
+    return (
+        <header className="w-full bg-white border-b border-gray-200 sticky top-0 z-50">
+           <div className="max-w-6xl mx-auto px-4">
+
+
+            <div className="flex items-center justify-between h-14 ml-10">
+                <div className="flex items-center justify-between h-14 gap-8 ">
+                    <Link
+                    to="/"
+                    className="bg-blue-600 text-white font-bold text-2xl px-2 py-1 rounded"
+                    >
+                        in
+                    </Link>
+
+                    <div className="flex items-center bg-[#edf3f8] px-4 py-2 rounded-full w-[320px]">
+                        <Search size={18} className="text-gray-600"/>
+                        <input
+                        type='text'
+                        placeholder="Describe the job you want"
+                        className="bg-transparent outline-none px-2 text-sm w-full text-gray-700 placeholder-gray-500"
+                        />
+                    </div>
+                </div>
+
+            <nav className="flex items-center gap-8 text-gray-600">
+                <Link
+                to="/"
+                className="flex flex-col items-center text-xs hover:text-black"
+                >
+                    <Home size={26}/>
+                     <span>Home</span>
+                </Link>
+
+                <Link
+                to="/network"
+                className="flex flex-col items-center text-xs hover:text-black"
+                
+                >
+                    
+                    <Users size={26}/>
+                     <span>My Network</span>
+                </Link>
+
+                <Link
+                to="/jobs"
+                className="flex flex-col items-center text-xs hover:text-black"
+                >
+                    <BriefcaseBusiness size={26}/>
+                    <span>Jobs</span>
+                </Link>
+
+                <Link
+                to="/messages"
+                className="flex flex-col items-center text-xs hover:text-black"
+                >
+                    <MessageSquareText size={26}/>
+                     <span>Messages</span>
+                </Link>
+
+                <Link
+                to="/notifications"
+                className="relative flex flex-col items-center text-xs hover:text-black"
+                >
+                    <Bell size={26} />
+                    <span>Notifications</span>
+                    <span className="absolute -top-1 -right-4 bg-red-600 text-white text-[10px] px-1.5 rounded-full">
+                        13
+                    </span>
+                </Link>
+
+                <Link
+                to="/profile"
+                className="flex flex-col items-center text-xs hover:text-black"
+                >
+                    <img
+                    src="https://i.pravatar.cc/40"
+                    alt="profile"
+                    className="w-9 h-9 rounded-full"
+                    />
+                    <span>Me</span>
+                </Link>
+
+            </nav>
+            </div>
+           </div>
+        </header>
+    )
+}
--- linkedin/src/components/Navbar/Navbar.jsx
+++ linkedin/src/components/Navbar/Navbar.jsx
@@ -1,81 +1,14 @@
 import React from "react"
-import {Link} from "react-router-dom"
-import{Search,Home,Users,BriefcaseBusiness,MessageSquareText,Bell,User,} from "lucide-react"
-
+import DesktopNavbar from "./DesktopNavbar"
 
 export default function Navbar(){
     return (
-        <header>
-            <div>
-                <div>
-                    <div>
-                        <Link to="/">
-                        in
-                        </Link>
+        <>
+        
+        <div className="hidden md:block">
+            <DesktopNavbar/>
+        </div>
 
-                        <div>
-                            <Search size={18}/>
-                                <input 
-                                type="text"
-                                placeholder="Search"
-                                />
-                            
-                        </div>
-                    </div>
-
-                    <nav>
-                        <Link to="/">
-                        <Home size={20}/>
-                        <span>Home</span>
-                        </Link>
-
-                        <Link
-                        to="/network"
-                        >
-                            <Users size={20}/>
-                            <span>My Network</span>
-                        </Link>
-
-                        <Link
-                        to="/jobs"
-                        >
-                            <BriefcaseBusiness size={20}/>
-                            <span>jobs</span>
-                        </Link>
-
-                        <Link
-                        to="/message"
-                        >
-                            <MessageSquareText size={20}/>
-                            <span>Messaging</span>
-                        </Link>
-
-                        <Link
-                        to="/notofications"
-                        >
-                            <Bell size={20} />
-                            <span>Notifications</span>
-                        </Link>
-
-                        <Link
-                        to="/profile"
-                        >
-                            <user size={20} />
-                            <span>Me</span>
-                        </Link>
-                    </nav>
-                </div>
-
-                <div>
-                    <div>
-                        <Search size={18}/>
-                        <input
-                        type="text"
-                        placeholder="Search"
-                        />
-                    </div>
-                </div>
-            </div>
-        </header>
+        </>
     )
 }
