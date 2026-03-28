8ca8644562d11f2f4ef23d70659858be520971e8
shivesh93212 <0105cd231185@oriental.ac.in>
2026-03-28 17:52:33 +0530

    final

--- backend/main.py
+++ backend/main.py
@@ -27,11 +27,10 @@ app=FastAPI(title="ProLinker Backend")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173",
-                   "http://127.0.0.1:5173"],
+    allow_origins=["*"],   # temporary
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
 try:
